05e1e956b3c7f69fbc4deca12e383d224a854c6a
outlandi-co <outlandico@gmail.com>
2024-06-18 13:08:58 -0700

    file changes update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+// App.jsx
+
 import React from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import Categories from './Components/Categories/index'; // Correct casing
+import Categories from './Components/Categories/index'; // Assuming correct casing for Categories
 import Products from './Components/Products';
 import './App.scss';
 
